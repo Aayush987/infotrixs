2b215257543feaea25fbd120e798989358a2b2d1
Aayush <aayushsharma47163@gmail.com>
2023-11-08 14:38:06 +0530

    Random Quote Generator Project Added

---
+++ src/App.jsx
@@ -0,0 +1,93 @@
+import QuoteCard from "./components/QuoteCard";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+
+
+const App = () => {
+
+  const [Quote, setQuote] = useState("");
+  const [Author, setAuthor] = useState("");
+  const [Search, setSearch] = useState("");
+  const [Key, setKey] = useState("");
+  const [authorlist, setauthorlist] = useState([]);
+  const [buttonClicked, setButtonClicked] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+
+      const res = await axios.get(
+        "https://api.quotable.io/authors/"
+      );
+      const data = await res.data;
+      setauthorlist(data.results.map((data) => data.name));
+
+
+      try {
+        if (buttonClicked) {
+          const response = await axios.get(
+            `https://api.quotable.io/quotes?author=${Search}`
+          );
+          const data = await response.data;
+          setQuote(data.results.map((data) => data.content));
+          setKey(data.results.map((data)=>data._id));
+          setAuthor(data.results.map((data) => data.author));
+        } else {
+          const response = await axios.get(
+            "https://api.quotable.io/quotes/random/"
+          );
+          const data = response.data;
+          setQuote(data.map((data) => data.content));
+          setAuthor(data.map((data) => data.author));
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, [buttonClicked]);
+  
+  
+
+
+  return (
+    <div className="main">
+
+      <h1 id = "title">QUOTE OF THE DAY</h1>
+        
+       <div className="search">
+       <input type="text" placeholder="Enter Author's name.." onChange= {(e) => {setSearch(e.target.value)}} />
+       <button onClick={() => {setButtonClicked((prevState) => !prevState), console.log("clicked")}}>Search</button>
+       </div>
+
+       {buttonClicked ? (
+  <div>
+    {Quote.map((quote,index) => (
+      <div className="quote" key = {index}>
+      <QuoteCard key={index} quote={quote} author={Author[0]} />
+      </div>
+    ))}
+  </div>
+) : (
+  <div className="quote">
+  <QuoteCard quote={Quote} author={Author}  />
+  </div>
+)}    
+   
+   <div className="author-list">
+
+   </div>
+     <ul>
+        <h3>List Of Authors</h3>
+     {authorlist.map((author,index) => (
+        <li key = {index}>{author}</li>
+     ))}
+     <p>...and many more.</p>
+     </ul>
+    </div>
+  )
+}
+
+export default App
